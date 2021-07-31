d494c80fc7a148d2bd5b8771c3ed0657bc0bf222
brad1024 <harry1024harry1024@gmail.com>
2021-07-31 10:20:12 +0800

    0731

--- combat_functions.js
+++ combat_functions.js
@@ -14,46 +14,47 @@ exports.combat_report_process = function(Env, combat_report){
     reports = [];
     console.log("戰報數量:" + combat_report.length);
     for(var i =0 ; i < combat_report.length ; i++){
-      
+		
 		r = combat_report[i]
-      var num_of_troop = Env.roads[r["direction"]].army_location[r["location"]].length;
-      var num_of_enemy = Env.roads[r["direction"]].enemy_location[r["location"]].length;
-      // prepend 所以要倒著放
-      if(r["wall_damaged"]>0){
-         
-        
-		if(r["army_attack"]>0){
-		var msg = "弓箭手造成" + r["army_attack"] + "點傷害<br>該樹人剩下"+r["enemy_hp"]+"點血量<br>該戰場剩下敵人數:"+num_of_enemy+"<br>"; 
-		reports.push(msg);
-		}
-
-		var wall_msg = "<b>" + direct_dic[r["direction"]] + "方</b>城牆正在被攻擊<br>受到"+r["enemy_attack"]+"點傷害<br>";
-		reports.push(wall_msg);
-      }
-      
-      else{
-
-        var msg = "該戰場剩下士兵數:"+num_of_troop+"<br>該戰場剩下敵人數:"+num_of_enemy+"<br>";
-        reports.push(msg);
-
-        if(r["location"]>0){
-			var msg =  "位於"+"<b>" + direct_dic[r["direction"]] + "方</b>距城門"+r["location"]+"公里處發生戰爭<br>"
-        }
-        else{
-			var msg =  "位於"+"<b>" + direct_dic[r["direction"]] + "方</b>城門下方發生戰爭<br>"
-        }
-		for(var i in r["army_attack"]){
-			if(r["army_attack"][i]){
-				msg += i+"造成"+r["army_attack"][i]+"點傷害<br>" 
+		console.log(r)
+		var num_of_troop = Env.roads[r["direction"]].army_location[r["location"]].length;
+		var num_of_enemy = Env.roads[r["direction"]].enemy_location[r["location"]].length;
+		// prepend 所以要倒著放
+		if(r["wall_damaged"]>0){
+			
+			
+			if(r["army_attack"]>0){
+			var msg = "弓箭手造成" + r["army_attack"] + "點傷害<br>該樹人剩下"+r["enemy_hp"]+"點血量<br>該戰場剩下敵人數:"+num_of_enemy+"<br>"; 
+			reports.push(msg);
 			}
+
+			var wall_msg = "<b>" + direct_dic[r["direction"]] + "方</b>城牆正在被攻擊<br>受到"+r["enemy_attack"]+"點傷害<br>";
+			reports.push(wall_msg);
 		}
-		msg += "樹人造成"+r["enemy_attack"]+"點傷害<br>先鋒部隊剩餘血量為:"+r["army_hp"]+"，該樹人剩下"+r["enemy_hp"]+"點血量<br>";
-		reports.push(msg);
-  
-      
-        
-  
-      }
+		
+		else{
+
+			var msg = "該戰場剩下士兵數:"+num_of_troop+"<br>該戰場剩下敵人數:"+num_of_enemy+"<br>";
+			reports.push(msg);
+
+			if(r["location"]>0){
+				var msg =  "位於"+"<b>" + direct_dic[r["direction"]] + "方</b>距城門"+r["location"]+"公里處發生戰爭<br>"
+			}
+			else{
+				var msg =  "位於"+"<b>" + direct_dic[r["direction"]] + "方</b>城門下方發生戰爭<br>"
+			}
+			for(var i in r["army_attack"]){
+				if(r["army_attack"][i]){
+					msg += i+"造成"+r["army_attack"][i]+"點傷害<br>" 
+				}
+			}
+			msg += "樹人造成"+r["enemy_attack"]+"點傷害<br>先鋒部隊剩餘血量為:"+r["army_hp"]+"，該樹人剩下"+r["enemy_hp"]+"點血量<br>";
+			reports.push(msg);
+	
+		
+			
+	
+		}
       
    
     }
@@ -72,7 +73,8 @@ exports.combat = function(Env, dir, total_report, defender_data){
 	var army_attack = {"armor":0, "archer":0, "ranger":0, "defence":0};
 	//var defence_attack = 0;
 	var total_damage = 0
-	var enemy_attack = 0;
+	var enemy_attack = {"tree_man":0, "big_tree_man":0, "stick_man":0};
+	var enemy_total_damage = 0
 	var isCombat = false;
 	var farest_army = Env.roads[dir].farest_army;
 	var nearest_enemy = Env.roads[dir].nearest_enemy;
@@ -118,11 +120,17 @@ exports.combat = function(Env, dir, total_report, defender_data){
 				enemy_attack += Env.roads[dir].enemy_location[i][j].attack;
 			}
 			else if(farest_army!=-1 && i - Env.roads[dir].enemy_location[i][j].attack_range <= farest_army){
-				enemy_attack += Env.roads[dir].enemy_location[i][j].attack;
+				var enemy_type = Env.roads[dir].enemy_location[i][j].type
+				enemy_attack[enemy_type] += Env.roads[dir].enemy_location[i][j].attack;
 			}
 		}
     }
-    if(total_damage || enemy_attack){
+	for(var i in enemy_attack){
+		enemy_total_damage += enemy_attack[i]
+	}
+
+
+    if(total_damage || enemy_total_damage){
       	isCombat = true;
     }
   
@@ -132,6 +140,7 @@ exports.combat = function(Env, dir, total_report, defender_data){
 		"wall_damaged":false, //城牆是否被攻擊
 		"total_damage":total_damage, //部隊造成的傷害
 		"army_attack":army_attack,
+		"enemy_total_damage":enemy_total_damage,
 		"enemy_attack":enemy_attack, //樹人造成的傷害
 		"army_hp":farest_army_hp, //最前方部隊剩餘血量
 		"enemy_hp": nearest_enemy_hp, //最近樹人剩餘血量
@@ -141,13 +150,13 @@ exports.combat = function(Env, dir, total_report, defender_data){
     //console.log(isCombat);
     if(isCombat){
 
-		if(farest_army==-1 && enemy_attack!=0){
+		if(farest_army==-1 && enemy_total_damage!=0){
 			//console.log("樹人到城牆下啦");
-			Env.roads[dir].wallhp = Math.max(0, Env.roads[dir].wallhp-enemy_attack);
+			Env.roads[dir].wallhp = Math.max(0, Env.roads[dir].wallhp-enemy_total_damage);
 			combat_detail["wall_damaged"] = true;
 		}
 		else if(farest_army!=-1){
-			farest_army_hp = Math.max(0, farest_army_hp - enemy_attack);
+			farest_army_hp = Math.max(0, farest_army_hp - enemy_total_damage);
 			combat_detail["army_hp"] = farest_army_hp;
 			combat_detail["location"] = farest_army;
 			Env.roads[dir].army_location[farest_army][0].hp = farest_army_hp;
